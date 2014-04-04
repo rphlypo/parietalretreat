5df24ba14142db4a1747f263655e49afbed52ac8
Ronald Phlypo <ronald.phlypo@inria.fr>
2014-04-04 10:17:55 +0200

    load_data.py functional

--- load_data.py
+++ load_data.py
@@ -1,44 +1,66 @@
 import os.path
+import string
 
+
+from nipy.modalities.fmri import hrf, utils
 from collections import Iterable
-from nilearn.input_data import MultiNiftiMasker
 
 
-def _check_arguments(data_dict):
+hrf_func = utils.lambdify_t(hrf.glover(utils.T))
+
+
+def _get_paths(data_dict):
+    path = dict()
+    if not isinstance(data_dict, dict):
+        raise ValueError("'data_dict' must be a dictionary")
+    if not "func" in data_dict.keys():
+        raise LookupError("at least 'func' must appear as a key in data_dict"
+                          " to allow localisation of the bold images")
+
+    _check_args(data_dict, data_type="func")
+    path["func"] = os.path.normpath(data_dict["func"].format(**data_dict))
+
+    # Confounds
+    for data_type in {"conf", "anat", "onsets"} & set(data_dict.keys()):
+        _check_args(data_dict, data_type=data_type)
+        path[data_type] = os.path.normpath(
+            data_dict[data_type].format(**data_dict))
+
+    return path
+
+
+def _check_args(data_dict, data_type="func"):
     str_out = ""
-    if "dataset" not in data_dict.keys():
-        raise LookupError("'dataset' has not been defined by user")
-    if data_dict["dataset"] == "hcp":
-        # Argument check
-        _check_data_dict()
-        for key in {"base_dir", "task_id", "scan_mode", "subj_id"}:
-            if not key in data_dict.keys():
-                str_out += ", " + key if len(str_out) >= 1 else key
-
-        if len(str_out) >= 1:
-            raise LookupError("Missing arguments for dataset '{}': ".format(
-                data_dict["dataset"]) + str_out)
-
-        data_path = os.path.join(data_dict["base_dir"],
-                                 "HCP/Q2",
-                                 format(data_dict["subj_id"]),
-                                 "MNINonLinear/Results",
-                                 "tfMRI_{}_{}".format(data_dict["task_id"],
-                                                      data_dict["scan_mode"]),
-                                 "tfMRI_{}_{}.nii.gz".format(
-                                     data_dict["task_id"],
-                                     data_dict["scan_mode"]))
-
-        # Confounds
-
-    return data_path
+    str_format = string.Formatter()
+    print list(str_format.parse(data_dict[data_type]))
+    needed_keys = zip(*list(str_format.parse(data_dict[data_type])))[1]
+    if needed_keys[-1] is None:
+        needed_keys = needed_keys[:-1]
+    for key in needed_keys:
+        str_out += format(key) if str_out == "" else ", " + format(key)
+    if not all([key in data_dict.keys() for key in needed_keys]):
+        raise LookupError("format key is missing in argument list: "
+                          "necessary format keys are " + str_out)
+    return True
 
 
-def get_paths(func):
-    def check_data_set(niimgs):
-        if isinstance(niimgs, Iterable):
-            return [check_data_set(niimg) for niimg in niimgs]
-        else:
-            return _check_arguments(niimgs)
+def dict_2_paths(arg_list):
+    """
+    Given a (list of) dictionary(ies), evaluate the path strings
 
-MultiNiftiMasker.fit = get_paths(MultiNiftiMasker.fit)
+    parameters:
+    ----------
+    arg_list: either an iterable of dict objects or a single dict object
+        make sure the "func" key appears in the dictionaries which is an
+        unformatted string, using the arguments appearing in the dictionary
+        allowed paths are "func", "anat", "onsets", "confds"
+
+    output:
+    ------
+    paths: a (list of) dictionary(ies)
+        paths that are formatted using the arguments from the dictionaries
+    """
+    if isinstance(arg_list, Iterable) and not isinstance(arg_list, dict):
+        return [dict_2_paths(arg) for arg in arg_list]
+    else:
+        return _get_paths(arg_list)
